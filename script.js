bcc06eebfb148ab2ecac813086d1c5feb1ba0ae7
NIRANJAN S <niranjansuresh428@gmail.com>
2025-10-07 20:09:39 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,30 +2,26 @@ document.addEventListener("DOMContentLoaded", function () {
     const openButton = document.getElementById("open-button");
     const initialDialog = document.getElementById("initial-dialog");
     const mainContent = document.getElementById("main-content");
+    const readMoreButton = document.getElementById("read-more-button");
+    const longMessageDialog = document.getElementById("long-message-dialog");
+    const closeMessageButton = document.getElementById("close-message-button");
     const birthdaySong = document.getElementById("birthday-song");
 
+    // When the user clicks "Open"
     openButton.addEventListener("click", function () {
         initialDialog.classList.add("hidden");
         mainContent.classList.remove("hidden");
         birthdaySong.play();
     });
-});
-
-    // When the user clicks "Open"
-    openButton.addEventListener('click', () => {
-        initialDialog.classList.add('hidden');
-        mainContent.classList.remove('hidden');
-    });
 
     // When the user clicks "Read More"
-    readMoreButton.addEventListener('click', () => {
-        longMessageDialog.classList.remove('hidden');
-        // Play the song
+    readMoreButton.addEventListener("click", function () {
+        longMessageDialog.classList.remove("hidden");
         birthdaySong.play();
     });
 
     // When the user clicks "Close" on the long message
-    closeMessageButton.addEventListener('click', () => {
-        longMessageDialog.classList.add('hidden');
+    closeMessageButton.addEventListener("click", function () {
+        longMessageDialog.classList.add("hidden");
     });
 });
